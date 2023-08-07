5f928626bbd118a6d1d12a8bce9048bb4ba67c04
Yash-Bavishi <bavishiys63@gmail.com>
2023-08-08 00:37:29 +0530

    LETS TRY

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv'
 import authServer from './routes/authRoute.js'
 import shopRoute from './routes/shopRoute.js'
 import cartRoute from './routes/cartRoute.js'
-import {connectDB} from './config/db.js'
+import { connectDB } from './config/db.js'
 import { errorHandler } from './middleware/errorHandler.js';
 import cookies from 'cookie-parser'
 dotenv.config()
@@ -19,6 +19,14 @@ app.use('/api/user', authServer)
 app.use('/api/shop', shopRoute)
 app.use('/api/cart', cartRoute)
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')))
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+	app.get('/', (req, res) => res.send('Please set to production'))
+}
+
 app.use(errorHandler)
 
-app.listen(PORT,() => console.log(`Server started on ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
